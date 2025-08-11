cdfe1344a5baef20d4e201a190904569a0382e4b
Yury123Poltaran <yurapoltaranin@gmail.com>
2025-08-11 17:34:57 +0300

    chore(cypress): register db:clear/db:seed tasks in setupNodeEvents

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,28 @@
+/* eslint-disable max-len */
+// cypress.config.js
 const { defineConfig } = require('cypress');
+const { execSync } = require('child_process');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
-    video: false,
-    viewportWidth: 1280,
-    viewportHeight: 800,
+
+    setupNodeEvents(on, config) {
+      // РЕГИСТРАЦИЯ ТАСКОВ ДЛЯ CI/ЛОКАЛЬНЫХ ТЕСТОВ
+      on('task', {
+        'db:clear'() {
+          execSync('node bin/generate-demo-data.js --empty', { stdio: 'inherit' });
+          return null;
+        },
+        'db:seed'() {
+          execSync('node bin/generate-demo-data.js', { stdio: 'inherit' });
+          return null;
+        },
+      });
+
+      return config;
+    },
   },
 });
